326abcf605dd7a928cfdcf247769a2cc9913f215
Ayomide Oladele <fawaleayomideoladele@gmail.com>
2023-12-13 19:07:24 +0100

    main update

--- src/Section/Home.js
+++ src/Section/Home.js
@@ -1,11 +1,11 @@
-import {useEffect} from "react";
+import { useEffect } from "react";
 import Aos from "aos";
-import 'aos/dist/aos.css'
+import "aos/dist/aos.css";
 import Nav from "./Nav";
-import Bg from '../images/space_-_5200.mp4';
+import Bg from "../images/space_-_5200.mp4";
+import Idan from "../images/IMG-20221116-WA0005.jpg";
 
 const Home = () => {
-
   useEffect(() => {
     Aos.init({
       duration: 400,
@@ -14,16 +14,21 @@ const Home = () => {
     });
   });
 
-  return <div>
-    <div className="container">
-      <div className="overlay"></div>
-      <div className="content">
-      <Nav />
-
+  return (
+    <div>
+      <div className="container">
+        <div className="overlay"></div>
+        <div className="content">
+          <Nav />
+          <main>
+            <h1>Frontend Developer</h1>
+            <img src={Idan} alt="hero-img" className="hero" />
+          </main>
+        </div>
+        <video src={Bg} muted autoPlay loop></video>
       </div>
-      <video src={Bg} muted autoPlay loop></video>
     </div>
-    </div>;
+  );
 };
 
 export default Home;
