1be32536a81d391f300d61eb212c3cd182b8d55f
Ayomide Oladele <fawaleayomideoladele@gmail.com>
2023-12-21 20:31:08 +0100

    contact update

--- src/Section/Contact.js
+++ src/Section/Contact.js
@@ -1,20 +1,37 @@
-import {useRef} from 'react'
-import Nav from './Nav'
+import {useRef, useState} from 'react'
 import emailjs from '@emailjs/browser';
+import Nav from './Nav'
 
 const Contact = () => {
-  const form = useRef();
+  // const form = useRef();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('')
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_f68xsa5', 'template_czcf29p', form.current, '2nacB7w38X-FzEXNm')
-      .then((result) => {
-          console.log(result.text);
+    emailjs.sendForm('service_f68xsa5', 'template_mkpnzfd',
+    e.target, '2nacB7w38X-FzEXNm')
+    .then((result) => {
+      console.log(result.text);
       }, (error) => {
-          console.log(error.text);
-      });
-  };
+        console.log(error.text);
+        });
+        alert("Your message has been sent!")
+        // document.getElementById('contact-form').reset()
+        }
+
+
+    // emailjs.sendForm('service_f68xsa5', 'template_czcf29p', e.target, '2nacB7w38X-FzEXNm')
+    //   .then((result) => {
+    //       console.log(result.text);
+    //   }, (error) => {
+    //       console.log(error.text);
+    //   });
+  // };
+
   return (
     <div className='contact-container'>
       <Nav/>
@@ -23,13 +40,13 @@ const Contact = () => {
 
       <form
         onSubmit={sendEmail}
-        ref={sendEmail}
+        // ref={sendEmail}
       >
         <div class="inputBox">
           <input type="text" name='from_name' id="name" placeholder="Full Name" required />
           <input type="text" name='from_email' id="email" placeholder="Email Address" required />
         </div>
-        <div class="inputBox">
+        {/* <div class="inputBox">
           <input
             type="number"
             name='message'
@@ -38,9 +55,9 @@ const Contact = () => {
             required
           />
           <input type="text" id="email-sub" placeholder="Email Subject" />
-        </div>
+        </div> */}
         <textarea
-          name=""
+          name="message"
           id="message"
           cols="30"
           rows="10"
