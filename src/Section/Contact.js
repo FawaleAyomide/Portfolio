eb5d31d0ef6b7c528b9cfcdb5ca423d60506a25f
Ayomide Oladele <fawaleayomideoladele@gmail.com>
2023-12-22 02:52:23 +0100

    contact update

--- src/Section/Contact.js
+++ src/Section/Contact.js
@@ -1,52 +1,74 @@
-import {useRef, useState} from 'react'
-import emailjs from '@emailjs/browser';
-import Nav from './Nav'
+import { useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import emailjs from "@emailjs/browser";
+import Nav from "./Nav";
+
+const notify = () => {
+  toast.success("Sent Successfully", {
+    theme: "dark",
+    position: "top-center",
+    autoClose: 2500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
 
 const Contact = () => {
-  // const form = useRef();
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('')
+  const reset = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs.sendForm('service_f68xsa5', 'template_mkpnzfd',
-    e.target, '2nacB7w38X-FzEXNm')
-    .then((result) => {
-      console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-        });
-        alert("Your message has been sent!")
-        // document.getElementById('contact-form').reset()
+    e.target, 'puAGSj6EpJVsWTtha')
+    // emailjs
+    //   .sendForm(
+    //     "process.env.React_App_Service_Id",
+    //     "process.env.React_App_Template_Id",
+    //     e.target,
+    //     "process.env.React_App_Public_Id"
+    //   )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
         }
-
-
-    // emailjs.sendForm('service_f68xsa5', 'template_czcf29p', e.target, '2nacB7w38X-FzEXNm')
-    //   .then((result) => {
-    //       console.log(result.text);
-    //   }, (error) => {
-    //       console.log(error.text);
-    //   });
-  // };
+      );
+    reset.current.value = "";
+  };
 
   return (
-    <div className='contact-container'>
-      <Nav/>
+    <div className="contact-container">
+      <Nav />
       <section class="contact" id="contact">
-      <h2 class="heading">Contact <span>Me!</span></h2>
+        <h2 class="heading">
+          Contact <span>Me!</span>
+        </h2>
 
-      <form
-        onSubmit={sendEmail}
-        // ref={sendEmail}
-      >
-        <div class="inputBox">
-          <input type="text" name='from_name' id="name" placeholder="Full Name" required />
-          <input type="text" name='from_email' id="email" placeholder="Email Address" required />
-        </div>
-        {/* <div class="inputBox">
+        <form onSubmit={sendEmail}>
+          <div class="inputBox" ref={reset}>
+            <input
+              type="text"
+              name="from_name"
+              id="name"
+              placeholder="Full Name"
+              required
+            />
+            <input
+              type="text"
+              name="from_email"
+              id="email"
+              placeholder="Email Address"
+              required
+            />
+          </div>
+          {/* <div class="inputBox">
           <input
             type="number"
             name='message'
@@ -56,18 +78,25 @@ const Contact = () => {
           />
           <input type="text" id="email-sub" placeholder="Email Subject" />
         </div> */}
-        <textarea
-          name="message"
-          id="message"
-          cols="30"
-          rows="10"
-          placeholder="Your Message"
-        ></textarea>
-        <input type="submit" value="Send" class="btn sub-btn" />
-      </form>
-    </section>
-      </div>
-  )
-}
+          <textarea
+            name="message"
+            id="message"
+            cols="10"
+            rows="5"
+            placeholder="Your Message"
+            ref={reset}
+          ></textarea>
+          <input
+            type="submit"
+            value="Send"
+            class="btn sub-btn"
+            onClick={notify}
+          />
+        </form>
+      </section>
+      <ToastContainer className="toast" />
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
